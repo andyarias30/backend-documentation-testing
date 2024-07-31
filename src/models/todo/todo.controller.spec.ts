3b7ab91d50684fa56a93da39180922986e9a9606
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2024-07-31 09:53:50 -0400

    improving code and structure

--- src/models/todo/todo.controller.spec.ts
+++ src/models/todo/todo.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TodoController } from './todo.controller';
+import { TodoController } from './controllers/todo.controller';
 
 describe('TodoController', () => {
   let controller: TodoController;
